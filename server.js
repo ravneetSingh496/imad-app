bf7f0786e77c5ccf54839b8fb7a693c8dc1fc1a6
ravneetSingh496 <rsingh46@hawk.iit.edu>
2017-08-05 21:25:28 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,7 +108,8 @@ app.get('/', function (req, res) {
 });
 
 var counter = 0;
-app.get('/counter', function (req, res) {
+app.get('/counter', function (req, res) 
+{
     counter = counter + 1;
     res.send(counter.toString());
 });
