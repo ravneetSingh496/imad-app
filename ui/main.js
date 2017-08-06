d2ba508d5b92338d09c0352f1ded86e6f846e2e7
ravneetSingh496 <rsingh46@hawk.iit.edu>
2017-08-05 22:00:18 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,19 @@ button.onclick = function()
     request.open('GET', 'http://rsingh46.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = funtion()
+{
+    var names = ['name1', 'name2', 'name3','name4'];   
+    var list = '';
+    for (var index = 0; index < names.length; index++)
+    {
+        list += '<li>' + names[index] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
+
