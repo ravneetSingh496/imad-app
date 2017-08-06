1eca993963020aaff314b794152203c7029851ea
ravneetSingh496 <rsingh46@hawk.iit.edu>
2017-08-05 21:05:19 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,23 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+
+button.onClick = function()
+{
+    var request = new XMLHttpRequest();
+    
+    reuest.onreadystatechange = function()
+    {
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            if(request.status === 200)
+            {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        
+    };
+    
+    request.open('GET', 'http://rsingh46.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
