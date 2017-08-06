18a3b7f4282b26c1e14fe1d52714c93d1806c24c
ravneetSingh496 <rsingh46@hawk.iit.edu>
2017-08-05 22:23:21 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,15 +19,30 @@ button.onclick = function()
     request.send(null);   
 };
 
+var nameInput = document.getElementById('name');
+var name1 = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function()
 {
-    var names = ['name1', 'name2', 'name3','name4'];   
-    var list = "";
-    for (var index = 0; index < names.length; index++)
+    var request = new XMLHttpRequest();
+    reuest.onreadystatechange = function()
     {
-        list += '<li>' + names[index] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list; 
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            if(request.status === 304)
+            {
+                var names = ['name1', 'name2', 'name3','name4'];   
+                var list = "";
+                for (var index = 0; index < names.length; index++)
+                {
+                    list += '<li>' + names[index] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list; 
+            }
+        }
+    };
+    
+    request.open('GET', 'http://rsingh46.imad.hasura-app.io/submit-name?name=' + name1, true);
+    request.send(null);  
 };
